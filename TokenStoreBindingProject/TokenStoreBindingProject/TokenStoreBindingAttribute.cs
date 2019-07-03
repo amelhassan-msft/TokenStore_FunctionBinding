da119c4c94f61da11134074786f8fb12c7c4a47e
Amel Hassan <t-amhass@microsoft.com>
2019-07-03 12:08:11 -0700

    Updated usage description

--- TokenStoreBindingProject/TokenStoreBindingProject/TokenStoreBindingAttribute.cs
+++ TokenStoreBindingProject/TokenStoreBindingProject/TokenStoreBindingAttribute.cs
@@ -1,19 +1,20 @@
 ﻿/*
  //////// Token Store Binding //////////
 
- Usage: 
+Usage: 
 
 If Auth_Flag = MSI
-    1. Token Store can be used as an imperative binding or a declarative binding (i.e. can be used as direct input param)
-    2. Token_url should be the full path to the token
-    3. Req can be NULL
+    1. Token Store can be used as an imperative binding or a declarative binding (i.e. can be used as direct input param to your Azure Function)
+    2. Token_url should be the full path to the token ( i.e. https://{example-tokenstore-name}.{example-location}.tokenstore.azure.net/services/{example-service}/tokens/{example-token-name} )
+    3. Do NOT set the "Req" parameter 
 If Auth_Flag = user
     1. Token Store binding can ONLY be used as an imperative binding  
     2. Your Azure Function must have an HTTP trigger 
     3. Token Store binding can only be executed at runtime because the req parameters depends on the HTTP trigger output to access the headers 
-    4. Token_url should be path to service (i.e. dropbox) (without a trailing slash) 
+    4. Token_url should be path upto service ( i.e. https://{example-tokenstore-name}.{example-location}.tokenstore.azure.net/services/{example-service} )
 
-Reference: https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-class-library
+Reference (declartive vs. imperative bindings): https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-class-library
+
 */
 
 namespace Microsoft.Azure.WebJobs
