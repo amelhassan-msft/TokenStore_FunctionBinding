3e596816abbb1df6d6879eda6ff103a5934e45ed
Amel Hassan <t-amhass@microsoft.com>
2019-07-29 12:00:39 -0700

    Modularized code and added google id provider

--- TokenStoreBindingProject/TokenStoreBindingProject/TokenStoreBindingAttribute.cs
+++ TokenStoreBindingProject/TokenStoreBindingProject/TokenStoreBindingAttribute.cs
@@ -39,13 +39,14 @@ namespace Microsoft.Azure.WebJobs
         [AutoResolve]
         public string Token_url { get; set; }
         [AutoResolve (Default = "msi")]
-        public string Auth_flag { get; set; }
+        public string Auth_flag { get; set; } // options: msi or user 
+        public ID_Providers Identity_provider { get; set; } // options: aad, google, or facebook (see enum definition below) 
 
-
-        public TokenStoreBindingAttribute(string Token_url_in, string Auth_flag_in) // For imperative bindings, constructor
+        public TokenStoreBindingAttribute(string Token_url_in, string Auth_flag_in, ID_Providers Identity_provider_in) // For imperative bindings, constructor
         {
             Token_url = Token_url_in;
             Auth_flag = Auth_flag_in;
+            Identity_provider = Identity_provider_in;
         }
         public TokenStoreBindingAttribute() // For declarative bindings, constructor
         {
@@ -56,30 +57,53 @@ namespace Microsoft.Azure.WebJobs
         {
             public string TemplateBind(PropertyInfo propInfo, Attribute resolvedAttribute, BindingTemplate bindingTemplate, IReadOnlyDictionary<string, object> bindingData) // most important params are resolvedAttribute and bindingData 
             {
+
                 var tokenAttribute = resolvedAttribute as TokenStoreBindingAttribute;
                 if (tokenAttribute == null) // If token store binding attribute is NULL 
                 {
                     throw new InvalidOperationException($"Can not use {nameof(EasyAuthAccessTokenResolutionPolicy)} as a resolution policy for an attribute that does not implement {nameof(TokenStoreBindingAttribute)}");
                 }
 
-                if (!(bindingData.ContainsKey("$request") && bindingData["$request"] is HttpRequest)) // If we can't get "request" from http request
+                if (tokenAttribute.Auth_flag.ToLower() == "user")
                 {
-                    throw new InvalidOperationException($"Can not use \"Auth_Flag = user\" with a non-HTTP triggered function.");
+                    if (!(bindingData.ContainsKey("$request") && bindingData["$request"] is HttpRequest)) // If we can't get "request" from http request
+                    {
+                        throw new InvalidOperationException($"Http request not accessible. An Auth_flag of user requires the use of an Http triggered function.");
+                    }
+                    var request = (HttpRequest)bindingData["$request"];
+                    return GetEasyAuthAccessToken(request, tokenAttribute.Identity_provider);  // returns to EasyAuthAccessToken variable 
                 }
-                var request = (HttpRequest)bindingData["$request"];
-                return GetEasyAuthAccessToken(request);  // returns to EasyAuthAccessToken variable 
+                return "NULL"; // Header value is not required for an Auth_flag of "msi" 
             }
 
-            private string GetEasyAuthAccessToken(HttpRequest request)
+            private string GetEasyAuthAccessToken(HttpRequest request, ID_Providers Identity_provider)
             {
                 string errorMessage = "Failed accessing request header. Cannot find an access token for the user. Verify that this endpoint is protected by Azure App Service Authentication/Authorization.";
                 StringValues headerValues;
-                if (request.Headers.TryGetValue("X-MS-TOKEN-AAD-ID-TOKEN", out headerValues)) // TODO: get header depending on provider used to sign in users (i.e. AAD, facebook, google...)
-                    return headerValues.ToString(); 
+
+                switch(Identity_provider) // TODO: Enums should not be case sensitive, also in extension provider make sure to parse the token accordingly (remove @ if necessary) 
+                {
+                    case ID_Providers.aad:
+                        if (request.Headers.TryGetValue("X-MS-TOKEN-AAD-ID-TOKEN", out headerValues)) 
+                            return headerValues.ToString();
+                        break;
+                    case ID_Providers.facebook:
+                        if (request.Headers.TryGetValue("X-MS-TOKEN-FACEBOOK-ACCESS-TOKEN", out headerValues))
+                            return headerValues.ToString();
+                        break;
+                    case ID_Providers.google:
+                        if (request.Headers.TryGetValue("X-MS-TOKEN-GOOGLE-ID-TOKEN", out headerValues))
+                            return headerValues.ToString();
+                        break;
+                    default:
+                        throw new InvalidOperationException("Incorrect usage of Identity_provider parameter. Input must be of type ID_providers enum which currently supports aad, facebook, and google");
+                }
                 throw new InvalidOperationException(errorMessage);
             }
-
+  
         }
 
     }
+
+    public enum ID_Providers { aad, facebook, google }
 }
--- TokenStoreBindingProject/TokenStoreBindingProject/TokenStoreBinding_ExtensionProvider.cs
+++ TokenStoreBindingProject/TokenStoreBindingProject/TokenStoreBinding_ExtensionProvider.cs
@@ -21,33 +21,131 @@ using System.Text;
 [Extension("TokenStoreTest")]
 public class TokenStoreBinding_ExtensionProvider : IExtensionConfigProvider
 {
-
     public void Initialize(ExtensionConfigContext context)
     {
         var rule = context.AddBindingRule<TokenStoreBindingAttribute>();
         rule.BindToInput<String>(BuildItemFromAttribute); // <{output type of binding}>
     }
 
+    // ******************************************** Token Store Helper Functions ********************************************************
+    public async Task<String> get_or_create_token(string tokenResourceUrl, string tokenStoreResource)
+    {
+        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+        string tokenStoreApiToken = await azureServiceTokenProvider.GetAccessTokenAsync(tokenStoreResource); // Get a token to access Token Store
+
+        // Get token from Token Store
+        var request = new HttpRequestMessage(HttpMethod.Get, tokenResourceUrl);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenStoreApiToken);
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        Token tokenStoreToken;
+        if (response.IsSuccessStatusCode) // Specified token exists and msi has permission to "get" tokens  
+        {
+            var serviceApiToken = await response.Content.ReadAsStringAsync();
+            tokenStoreToken = JsonConvert.DeserializeObject<Token>(serviceApiToken); // Access token to specified service 
+
+            try // Get token if it exists
+            {
+                return tokenStoreToken.Value.AccessToken;
+            }
+
+            catch
+            {
+                throw new ArgumentException($"Token Store token status message: {tokenStoreToken.Status.Error.Message}"); // If token already exists, but is not authenticated 
+            }
+        }
+        else // Specified token does not exist or msi does not have permission to "get" tokens, so try to create a token keeping in mind that msi might not have permission to "create" tokens 
+            create_token(tokenResourceUrl, tokenStoreApiToken, tokenStoreResource);
+
+        return null; // output token could not be accessed 
+    }
+    public async void create_token(string tokenResourceUrl, string tokenStoreApiToken, string tokenStoreResource)
+    {
+        HttpClient client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Put, tokenResourceUrl);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenStoreApiToken);
+
+        // Get token name based on url 
+        int pos = tokenResourceUrl.LastIndexOf("/") + 1;
+        var tokenId = tokenResourceUrl.Substring(pos, tokenResourceUrl.Length - pos); // TODO: Should the display name differ from the token name ? 
+ 
+        var requestContent = JObject.FromObject(new
+        {
+            displayName = tokenId
+        });
+
+        request.Content = new StringContent(requestContent.ToString(), Encoding.UTF8, "application/json");
+        var response = await client.SendAsync(request);
+        var responseStr = await response.Content.ReadAsStringAsync(); // Provides details of created token 
+
+        if (response.IsSuccessStatusCode) // Token was succesfully created 
+            throw new ArgumentException($"Http response message status code: {response.StatusCode.ToString()}. Specified token does not exist. A new token with displayname {tokenId} was created. Navagiate to your Token Store to login. Further details: {responseStr}");
+        else  // msi does not have permission to create tokens in this token store 
+            throw new ArgumentException($"Http response message status code: {response.StatusCode.ToString()}. MSI does not have \"create\" permission for the following token store: {tokenStoreResource}. Further details: {responseStr}");
+        
+        //tokenStoreToken = JsonConvert.DeserializeObject<Token>(responseStr);
+        //outputToken = tokenStoreToken.Value.AccessToken;
+        // would it be possible to redirect user to something like this: https://ameltokenstore.tokenstore.azure.net/services/dropbox/tokens/a86c35dd-68a5-4668-9fae-2e7743c4feef/login
+    }
+
+    // ***************************************************************************************************************************************************
+    // ***************************** Functions to deal with headers and extract token name based on ID Provider ******************************************
+    public string get_aad_tokenpath(JwtPayload header_payload, TokenStoreBindingAttribute arg) // Token name based on tenant and object IDs 
+    {
+        string tenantID = null;
+        string objectID = null;
+
+        foreach (Claim claim in header_payload.Claims)
+        {
+            if (claim.Type == "tid")
+                tenantID = claim.Value;
+            if (claim.Type == "oid")
+                objectID = claim.Value;
+        }
+
+        if (tenantID == null)
+            throw new ArgumentException("AAD Token Error: TenantID cannot be read");
+        else if (objectID == null)
+            throw new ArgumentException("AAD Token Error: ObjectID cannot be read");
+        else
+            return $"{arg.Token_url}/tokens/{objectID}"; // uriToken, TODO: Naming convention should be {tenantID}-{objectID} (currently too long)
+    }
+
+    public string get_facebook_tokenpath(JwtPayload header_payload, TokenStoreBindingAttribute arg)
+    {
+        // Token name based on user name 
+        return "NULL";
+    }
+
+    public string get_google_tokenpath(JwtPayload header_payload, TokenStoreBindingAttribute arg) // Token name based on sub (i.e. the google user id) 
+    {
+        string user_id = null;
+
+        foreach (Claim claim in header_payload.Claims)
+        {
+            if (claim.Type == "sub")
+                user_id = claim.Value;
+        }
+        return $"{arg.Token_url}/tokens/{user_id}";
+    }
+
+    // ********************************************************************************************************************
+
+    // ******************************************* Main Function ***********************************************************
     private async Task<String> BuildItemFromAttribute(TokenStoreBindingAttribute arg, ValueBindingContext arg2)
     {
         // Extract resource url from provided url path to token 
         Uri tokenURI = new Uri(arg.Token_url);
         string path = tokenURI.Authority;
         string tokenStoreResource = $"https://{tokenURI.Authority}"; // Format: "https://{token-store-name}.tokenstore.azure.net"
-        string tokenResourceUrl = arg.Token_url; 
-
-        string outputToken = null;
+        string tokenResourceUrl = arg.Token_url; // for user scienario only specify path up to service 
 
         try
         {
-            string IDToken = null;
-            // Extract tenant and object ID 
-            string tenantID = null;
-            string objectID = null;
-
             if (arg.Auth_flag.ToLower() == "user") // If Flag = "USER" or "user" 
             {
-                IDToken = arg.EasyAuthAccessToken; // this is an AAD ID header token 
+                string IDToken = arg.EasyAuthAccessToken; // access or ID token retrieved from header
                 JwtSecurityTokenHandler JwtHandler = new JwtSecurityTokenHandler();
                 if (!JwtHandler.CanReadToken(IDToken)) 
                     throw new ArgumentException("ID token cannot be read as JWT");
@@ -55,82 +153,41 @@ public class TokenStoreBinding_ExtensionProvider : IExtensionConfigProvider
                 var securityToken = JwtHandler.ReadJwtToken(IDToken);
                 var payload = securityToken.Payload; // extract payload data 
 
-                foreach (Claim claim in payload.Claims)
+                // Build path to token 
+                switch (arg.Identity_provider) // TODO: Enums should not be case sensitive
                 {
-                    if (claim.Type == "tid")
-                        tenantID = claim.Value;
-                    if (claim.Type == "oid")
-                        objectID = claim.Value;
+                    case ID_Providers.aad:
+                        tokenResourceUrl = get_aad_tokenpath(payload, arg);
+                        break;
+                    case ID_Providers.facebook:
+                        tokenResourceUrl = get_facebook_tokenpath(payload, arg);
+                        break;
+                    case ID_Providers.google:
+                        tokenResourceUrl = get_google_tokenpath(payload, arg);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Incorrect usage of Identity_provider parameter. Input must be of type ID_providers enum which currently supports aad, facebook, and google");
                 }
-
-                if (tenantID == null)
-                    throw new ArgumentException("TenantID cannot be read");
-                else if (objectID == null)
-                    throw new ArgumentException("ObjectID cannot be read");
-                else
-                    tokenResourceUrl = $"{arg.Token_url}/tokens/{objectID}"; // uriToken, TODO: Naming convention should be {tenantID}-{objectID} (currently too long)
             }
 
             // Shared logic for If Auth_Flag = "msi" or "user"
             if (arg.Auth_flag.ToLower() == "msi" || arg.Auth_flag.ToLower() == "user")
             {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                string tokenStoreApiToken = await azureServiceTokenProvider.GetAccessTokenAsync(tokenStoreResource); // Get a token to access Token Store
-
-                // Get token from Token Store
-                var request = new HttpRequestMessage(HttpMethod.Get, tokenResourceUrl);
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenStoreApiToken);
-                HttpClient client = new HttpClient();
-                HttpResponseMessage response = await client.SendAsync(request);
-
-                Token tokenStoreToken;
-                if (response.IsSuccessStatusCode) // Specified token exists 
-                {
-                    var serviceApiToken = await response.Content.ReadAsStringAsync(); 
-                    tokenStoreToken = JsonConvert.DeserializeObject<Token>(serviceApiToken); // Access token to specified service 
-
-                    try // Get token if it exists
-                    {
-                        outputToken = tokenStoreToken.Value.AccessToken;
-                        return outputToken;
-                    }
-                        
-                    catch
-                    {
-                        throw new ArgumentException($"Token Store token status message: {tokenStoreToken.Status.Error.Message}"); // If token already exists, but is not authenticated 
-                    }
-                }
-                else // Specified token does not exist, create token 
-                    request = new HttpRequestMessage(HttpMethod.Put, tokenResourceUrl);
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenStoreApiToken);
-
-                    var tokenId = "newtoken"; // TODO: FIX (make it more specific to the scienario?) -> Maybe a random display name instead ? 
-                    var requestContent = JObject.FromObject(new
-                    {
-                        displayName = tokenId 
-                    });
-
-                    request.Content = new StringContent(requestContent.ToString(), Encoding.UTF8, "application/json");
-                    var resp = await client.SendAsync(request);
-                    var responseStr = await resp.Content.ReadAsStringAsync(); // Provides details of created token 
-
-                    //tokenStoreToken = JsonConvert.DeserializeObject<Token>(responseStr);
-                    //outputToken = tokenStoreToken.Value.AccessToken;
-                    throw new ArgumentException($"Http response message status code: {response.StatusCode.ToString()}. Specified token does not exist. A new token with displayname {tokenId} was created. Navagiate to your Token Store to login. Further details: {responseStr}"); 
-                    // would it be possible to redirect user to something like this: https://ameltokenstore.tokenstore.azure.net/services/dropbox/tokens/a86c35dd-68a5-4668-9fae-2e7743c4feef/login
+                return await get_or_create_token(tokenResourceUrl, tokenStoreResource);
             }
-            else // error, incorrect flag usage 
+            else // error, incorrect usage of Auth_flag binding input  
                 throw new ArgumentException("Incorrect usage of Auth_flag binding input: Choose \"msi\" or \"user\" ");
         }
-        catch (Exception exp)
+        catch (Exception exp) // Overall catch statement 
         {
             throw new ArgumentException($"{exp}");
         }
 
     }
+
 }
 
-// Token json object definition 
+// *********************** Token json object definition ****************************************
 public class Token
 
 {
